1245a63a46569b83912fbd3652ef2769d6f3b69d
walkowae <walkowae@rose-hulman.edu>
2021-02-08 10:35:02 -0500

    login index formatted

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -30,7 +30,36 @@ rhit.ClassName = class {
 /* Main */
 /** function and class syntax examples */
 rhit.main = function () {
-	console.log("Ready");
+	console.log("Ready4");
+	rhit.startFirebaseUI();
+};
+
+rhit.startFirebaseUI = function () {
+	console.log("firebaseUI building");
+	//if (!this.fbAuthManager.isSignedIn) {
+
+	var uiConfig = {
+		signInSuccessUrl: '/homepage.html',
+		//signInSuccessUrl: function () {this.fbAuthManager.signIn();},
+		signInOptions: [
+			// Leave the lines as is for the providers you want to offer your users.
+			firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+			firebase.auth.EmailAuthProvider.PROVIDER_ID,
+			firebase.auth.PhoneAuthProvider.PROVIDER_ID,
+			firebaseui.auth.AnonymousAuthProvider.PROVIDER_ID
+		],
+		// tosUrl and privacyPolicyUrl accept either url string or a callback
+		// function.
+		// Terms of service url/callback.
+		tosUrl: '<your-tos-url>',
+		// Privacy policy url/callback.
+
+	};
+	// Initialize the FirebaseUI Widget using Firebase.
+	var ui = new firebaseui.auth.AuthUI(firebase.auth());
+	// The start method will wait until the DOM is loaded.
+	ui.start('#firebaseui-auth-container', uiConfig);
+	//}
 };
 
 rhit.main();
