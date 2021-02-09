5d48f4446b3071d76ba29bc0c90f5bfd289248b6
walkowae <walkowae@rose-hulman.edu>
2021-02-09 10:18:56 -0500

    Firebase updates page, Auth Started

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -11,6 +11,32 @@ var rhit = rhit || {};
 
 /** globals */
 rhit.variableName = "";
+rhit.ITEMS_COLLECTION = "Items"
+rhit.STORE_INFO = "StoreInfo"
+//?---------Store info Keys-----------------------------
+rhit.STORE_KEY_BUSINESS_NAME = "businessName";
+rhit.STORE_KEY_GENERAL_INFO = "generalInfo";
+rhit.STORE_KEY_LOGO = "logo";
+rhit.STORE_KEY_SLIDE_SHOW = "generalInfo";
+rhit.STORE_KEY_ANNOUNCEMENT_IMG = "announcementImg";
+rhit.STORE_KEY_ANNOUNCEMENT = "announcement";
+rhit.STORE_KEY_TAGLINE = "tagline";
+//?------------------------------------------------------
+rhit.spManager = null;
+
+
+// * ------ Store Info Object contains all information for Store Main Page -------------------------------
+rhit.StoreInfoObject = class {
+	constructor(businessName, generalInfo, logo, slideShow, announcementImg, announcement, tagline) {
+		this.businessName = businessName;
+		this.generalInfo = generalInfo;
+		this.logo = logo;
+		this.slideShow = slideShow;
+		this.announcementImg = announcementImg;
+		this.announcement = announcement;
+		this.tagline = tagline;
+	}
+}
 // !----------------------Store Page Controller ------------------------
 rhit.StorePageController = class {
 	constructor() {
@@ -18,9 +44,110 @@ rhit.StorePageController = class {
 
 			window.location.href = `/signin.html`;
 		});
+		rhit.spManager.beginListening(this.updateStoreInfo.bind(this));
+	}
+	updateStoreInfo() {
+		const store_info = rhit.spManager.setStoreInfo();
+		console.log('typeof store_info :>> ', store_info.businessName);
+		document.getElementById("businessNameFillerTitle").innerHTML = store_info.businessName;
+		document.getElementById("businessNameFillerA").innerHTML = `   ${store_info.businessName}`;
+		document.getElementById("businessNameFillerAbout").innerHTML = `ABOUT ${store_info.businessName}`;
+		document.getElementById("generalInfo").innerHTML = store_info.generalInfo;
 
 	}
+
 }
+// !--------------------Store Page Manager -------------------------------------
+rhit.SPManager = class {
+	constructor(uid) {
+		////console.log("list manager created");
+		// this._uid = rhit.fbAuthManager._user;
+		this._uid = uid;
+		this._documentSnapshot = {};
+		this._ref = firebase.firestore().collection(rhit.STORE_INFO);
+		this._unsubscribe = null;
+	}
+	stopListening() {
+		this._unsubscribe();
+	}
+
+	beginListening(changeListener) {
+		/*
+	
+		var query = this._ref;
+		////console.log('typeof :>> ', typeof this._ref);
+		if (this._uid) {
+			var query = this._ref.where(rhit.IMG_KEY_AUTHOR, "==", this._uid);
+		}
+		query.onSnapshot((querySnapshot) => {
+			this._documentSnapshot = querySnapshot.docs;
+			changeListener();
+		});
+		*/
+		this._ref.onSnapshot((querySnapshot) => {
+			this._documentSnapshot = querySnapshot.docs;
+			changeListener();
+		});
+
+	}
+
+	get length() {
+		return this._documentSnapshot.length;
+	}
+	setStoreInfo() {
+		console.log('typeof docSnapshot :>> ', this._documentSnapshot);
+		const docSnapshot = this._documentSnapshot[0];
+
+		const StoreInfo = new rhit.StoreInfoObject(
+			docSnapshot.get(rhit.STORE_KEY_BUSINESS_NAME),
+			docSnapshot.get(rhit.STORE_KEY_GENERAL_INFO),
+			docSnapshot.get(rhit.STORE_KEY_LOGO),
+			docSnapshot.get(rhit.STORE_KEY_SLIDE_SHOW),
+			docSnapshot.get(rhit.STORE_KEY_ANNOUNCEMENT_IMG),
+			docSnapshot.get(rhit.STORE_KEY_ANNOUNCEMENT),
+			docSnapshot.get(rhit.STORE_KEY_TAGLINE),
+
+		);
+		////console.log("images loaded");
+		return StoreInfo;
+	}
+};
+// !-----------------------------Firebase Auth Manager ------------------------
+rhit.FbAuthManager = class {
+	constructor() {
+		this._user = null;
+	}
+	beginListening(changeListener) {
+		firebase.auth().onAuthStateChanged((user) => {
+			this._user = user;
+			if (user) {
+
+				// User is signed in, see docs for a list of available properties
+				// https://firebase.google.com/docs/reference/js/firebase.User
+				//console.log(`ID :>>`, user.uid);
+
+				// ...
+			} else {
+				//console.log(`there is no user signed in`);
+				// User is signed out
+				// ...
+			}
+
+
+			changeListener();
+		});
+	}
+	signOut() {
+		firebase.auth().signOut();
+	}
+	get uid() {
+		return this._user.uid;
+	}
+	get isSignedIn() {
+		return !!this._user;
+	}
+}
+
 
 
 // ! ----------------------Intializing pages function-------------------------------------------
@@ -33,6 +160,7 @@ rhit.initializePage = function () {
 	}
 	if (document.querySelector("#storePage")) {
 		//console.log("On the login page");
+		rhit.spManager = new rhit.SPManager();
 		new rhit.StorePageController();
 	}
 
